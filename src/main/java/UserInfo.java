d04c7472b265059f585ad96c748503fbbf4ab244
bodanapusaileela-gl <puntikuradeepika@gmail.com>
2024-07-27 03:23:40 +0530

    Initial commit with Java application

---
+++ src/main/java/UserInfo.java
@@ -0,0 +1,12 @@
+// src/main/java/UserInfo.java
+public class UserInfo {
+    public static void main(String[] args) {
+        String name = "Saileela";
+        String job = "DevOps Engineer";
+        int packageNumber = 7;
+
+        System.out.println("User Name: " + name);
+        System.out.println("Job Title: " + job);
+        System.out.println("Package Number: " + packageNumber);
+    }
+}
